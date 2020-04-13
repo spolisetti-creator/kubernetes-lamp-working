39530ca14d545ef8fd331793d4e4a42a13df7e1e
root <root@ip-172-31-94-66.ec2.internal>
2020-04-13 17:42:51 +0000

    first commit

--- php7-apache2/mysql_test.php
+++ php7-apache2/mysql_test.php
@@ -1,24 +1,27 @@
+<html>
 <?php
-$host = getenv("DB_HOST");
-$db = getenv("DB_NAME");
-$user = getenv("DB_USER");
-$pwd = getenv("DB_PWD");
-$hostname = getenv("DB_HOST");
-$username = getenv("DB_USER");
-$password = getenv("DB_PWD");
-$db_name = getenv("DB_NAME");
-echo "start....";
-try{
-    $db=new PDO("mysql:host=".$hostname.";
-                dbname=".$db_name, $username, $password,
-                array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-    echo 'success........';
-    $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-    $db=null;
-}
-catch(PDOException $e){
-    echo $e->getMessage();
-    echo "error........";
-}
-echo "finish.....";
+//Step1
+$DB_HOST = 'mysql';
+$DB_NAME = getenv("DB_NAME");
+$DB_USER = 'root';
+$DB_PASSWORD = 'dummypassword';
+$conn = mysqli_connect($DB_HOST,$DB_USER,$DB_PASSWORD,$DB_NAME,3306)
+ or die('Error connecting to MySQL server.');
 ?>
+<body>
+<h1>Connected to MySQL </h1> 
+<h1>PHP connect to MySQL To demonstrate multi kubernetes containers and helm </h1>
+<h2> Displaying persons table data </h2>
+<?php
+//Step2
+$query = "SELECT * FROM Persons";
+mysqli_query($conn, $query) or die('Error querying database.');
+
+$result = mysqli_query($conn, $query);
+$row = mysqli_fetch_array($result);
+while ($row = mysqli_fetch_array($result)) {
+ echo $row['PersonID'] . ' ' . $row['LastName'] . ': ' . $row['FirstName'] . ' ' . $row['City'] .'<br />';
+}
+?>
+</body>
+</html>
